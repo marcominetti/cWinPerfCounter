4a6813e09a1220b7eb9d5230996d3ffa25be488e
Marco Minetti <marco.minetti@novetica.org>
2015-09-28 12:39:12 +0200

    Fixed module naming in index.js file.

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 // Attempt to load each of the available .node files for the current processor
 // architecture until one succeeds, or throw an exception if none do.
-var sModule = __dirname.match(/^.*[\/\\](.*?)$/)[1];
-module.exports = require("./build/Release/" + sModule + ".node")["cWinPerfCounter"];
+module.exports = require("./build/Release/cwinperfcounter-next.node")["cWinPerfCounter"];
